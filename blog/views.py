8f7df811f0cf82b7e6f8c6bf3ca5c939d068c07b
lzy <892431741@qq.com>
2020-05-20 10:04:44 +0800

    修改访问设置

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,60 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
-import markdown
-from django.utils.text import slugify
-from markdown.extensions.toc import TocExtension
-import re
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import DetailView, ListView
+
+#from pure_pagination.mixins import PaginationMixin
+
+from .models import Category, Post, Tag
 
 
-def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "post_list"
+    paginate_by = 10
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get("pk"))
+        return super().get_queryset().filter(category=cate)
 
-    # 阅读量 +1
-    post.increase_views()
 
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        # 记得在顶部引入 TocExtension 和 slugify
-        TocExtension(slugify=slugify),
-    ])
-    post.body = md.convert(post.body)
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__year=year, created_time__month=month)
+        )
 
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
 
-    return render(request, 'blog/detail.html', context={'post': post})
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get("pk"))
+        return super().get_queryset().filter(tags=t)
+
+
+# 记得在顶部导入 DetailView
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = "blog/detail.html"
+    context_object_name = "post"
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super().get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
