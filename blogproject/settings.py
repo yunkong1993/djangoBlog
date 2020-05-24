f30629479a3d32c3a3b91711991c9ea9c38d927a
efort-2004 <qQ892431741>
2020-05-24 21:31:32 +0800

    更新settings

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'secret_key.txt'))as f:
+with open(os.path.join(BASE_DIR, 'blogproject', 'secret_key.txt'))as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
