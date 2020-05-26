86aa018e7bf7f25219e591e011842c441d4c46b4
efort-2004 <892431741@qq.com>
2020-05-26 11:37:20 +0800

    修改urls

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-if settings.DEBUG:
-    # static files (images, css, javascript, etc.)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
