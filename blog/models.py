db476e382f85825c9f61803dc1ccbde38a36d0dc
efort-2004 <892431741@qq.com>
2020-05-26 09:49:57 +0800

    测试markdown

--- blog/models.py
+++ blog/models.py
@@ -93,7 +93,7 @@ class Post(models.Model):
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
         ])
-
+        self.body = md.convert(self.body)
         # 先将 Markdown 文本渲染成 HTML 文本
         # strip_tags 去掉 HTML 文本的全部 HTML 标签
         # 如果未填写摘要的话，从文本摘取前 54 个字符赋给 excerpt
