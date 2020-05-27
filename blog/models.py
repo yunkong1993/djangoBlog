00e25830416b273884c9987ccde1363f76af0064
efort-2004 <892431741@qq.com>
2020-05-27 20:46:43 +0800

    admin界面正文 body 改为 正文

--- blog/models.py
+++ blog/models.py
@@ -67,7 +67,7 @@ class Post(models.Model):
     """
     title = models.CharField('标题', max_length=70)
     # body = models.TextField('正文')
-    body = MDTextField()
+    body = MDTextField('正文')
     created_time = models.DateTimeField('创建时间', default=timezone.now)
     modified_time = models.DateTimeField('修改时间')
     excerpt = models.CharField('摘要', max_length=200, blank=True)
