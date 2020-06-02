33c6fc2e5c20badac1e3c0593525a54cfef6d99e
efort-2004 <892431741@qq.com>
2020-06-02 14:48:53 +0800

    修改后天管理页面标题和主页名

--- blog/admin.py
+++ blog/admin.py
@@ -11,6 +11,9 @@ class PostAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
+admin.site.site_header = '凌波浪子后台管理'  # 此处设置页面显示标题
+admin.site.site_title = '个人博客'  # 此处设置页面头部标题
+
 # 把新增的 Postadmin 也注册进来
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
